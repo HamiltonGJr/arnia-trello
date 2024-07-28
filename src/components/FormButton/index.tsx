f2edace4d24dc123d83cd976560284394cba8913
Hamilton Gonçalves <143231573+HamiltonGJr@users.noreply.github.com>
2024-07-28 15:13:19 -0300

    refactor: adjusting the import, of the button and the stack, to make the code more complete

--- src/components/FormButton/index.tsx
+++ src/components/FormButton/index.tsx
@@ -1,6 +1,5 @@
 import * as React from "react";
-import Stack from "@mui/material/Stack";
-import Button from "@mui/material/Button";
+import { Button, Stack } from "@mui/material";
 
 import { FormButtonProps } from "../../types/componetsType";
 
