1dfa402a862064082e440b01901c2a7dfef39723
Hamilton Gonçalves <143231573+HamiltonGJr@users.noreply.github.com>
2024-07-28 15:15:29 -0300

    style: adding general container styling

---
+++ src/styles/BoryStyles.ts
@@ -0,0 +1,11 @@
+import { styled } from "@mui/material/styles";
+import { Box } from "@mui/material";
+
+export const AppContainer = styled(Box)(({ theme }) => ({
+  height: "100vh",
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "center",
+  fontFamily: "Poppins",
+  backgroundColor: `${theme.palette.primary.light}`,
+}));
