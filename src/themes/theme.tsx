3661998e9751295bc4fc68ff90b1341b5f669085
Hamilton Gonçalves <143231573+HamiltonGJr@users.noreply.github.com>
2024-07-29 14:09:49 -0300

    style: adding a gray color to the theme

--- src/themes/theme.tsx
+++ src/themes/theme.tsx
@@ -1,14 +1,15 @@
 import { createTheme } from "@material-ui/core";
-import { red, deepPurple, yellow } from "@mui/material/colors";
+import { red, deepPurple, yellow, grey } from "@mui/material/colors";
 
 const theme = createTheme({
   palette: {
     primary: {
-      main: deepPurple[700],
-      light: deepPurple[400],
+      main: deepPurple[900],
+      light: deepPurple[100],
     },
     secondary: {
       main: yellow[500],
+      light: grey[100],
     },
     error: {
       main: red.A400,
