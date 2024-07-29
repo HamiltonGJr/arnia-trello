2052d35a80d225d97fb5ccc1ea0b0905757aaf09
Hamilton Gonçalves <143231573+HamiltonGJr@users.noreply.github.com>
2024-07-29 14:05:19 -0300

    style: input color adjustment

--- src/components/FormInput/style.tsx
+++ src/components/FormInput/style.tsx
@@ -11,5 +11,18 @@ export const FormInputText = styled(TextField)(({ theme }) => ({
     "&:hover fieldset": {
       borderColor: theme.palette.primary.main,
     },
+    "&.Mui-focused fieldset": {
+      borderColor: theme.palette.primary.main,
+    },
   },
+  "& .MuiInputLabel-root": {
+    backgroundColor: "#fff",
+    paddingLeft: "2px",
+    paddingRight: "2px",
+    borderRadius: "2px",
+  },
+  "& .MuiInputLabel-shrink": {
+    backgroundColor: "#fff",
+  },
+  backgroundColor: "#fff",
 }));
