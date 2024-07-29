139568e5627b394108533bc546d1ec756f8c8950
Hamilton Gonçalves <143231573+HamiltonGJr@users.noreply.github.com>
2024-07-29 14:08:53 -0300

    feat: starting the ToDoList page

---
+++ src/routes/ToDoList/index.tsx
@@ -0,0 +1,12 @@
+import * as React from "react";
+import Header from "../../components/Header";
+
+const ToDoList = () => {
+  return (
+    <>
+      <Header />
+    </>
+  );
+};
+
+export default ToDoList;
