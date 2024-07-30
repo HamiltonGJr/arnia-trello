6c6c9739e5d05b6a56af6e8c007b25d0c11c3cb3
Hamilton Gonçalves <143231573+HamiltonGJr@users.noreply.github.com>
2024-07-30 13:47:45 -0300

    feat: implementing password hiding next to the icon with an eye on imputs

--- src/components/FormInput/index.tsx
+++ src/components/FormInput/index.tsx
@@ -1,16 +1,58 @@
 import * as React from "react";
+import { FormControl, IconButton, InputAdornment } from "@mui/material";
+import { Visibility, VisibilityOff } from "@mui/icons-material";
 
 import { FormInputProps } from "../../types/componetsType";
-import { FormInputText } from "./style";
+import { FormInputLabel, FormInputText, FormOutlinedInputText } from "./style";
 
-const FormInput: React.FC<FormInputProps> = ({ nameInput }) => {
+const FormInput = ({ nameInput, isPassword }: FormInputProps) => {
+  const [showPassword, setShowPassword] = React.useState(false);
+
+  const handleClickShowPassword = () => setShowPassword((show) => !show);
+
+  const handleMouseDownPassword = (
+    event: React.MouseEvent<HTMLButtonElement>
+  ) => {
+    event.preventDefault();
+  };
+
   return (
-    <FormInputText
-      id="outlined-basic"
-      label={nameInput}
-      fullWidth
-      variant="outlined"
-    />
+    <>
+      {isPassword ? (
+        <>
+          <FormControl variant="outlined" fullWidth>
+            <FormInputLabel htmlFor="outlined-adornment-password">
+              {nameInput}
+            </FormInputLabel>
+            <FormOutlinedInputText
+              id="outlined-adornment-password"
+              type={showPassword ? "text" : "password"}
+              label="Senha"
+              endAdornment={
+                <InputAdornment position="end">
+                  <IconButton
+                    aria-label="toggle password visibility"
+                    onClick={handleClickShowPassword}
+                    onMouseDown={handleMouseDownPassword}
+                    edge="end"
+                  >
+                    {showPassword ? <VisibilityOff /> : <Visibility />}
+                  </IconButton>
+                </InputAdornment>
+              }
+            />
+          </FormControl>
+        </>
+      ) : (
+        <FormInputText
+          id="outlined-password-input"
+          label={nameInput}
+          type="text"
+          autoComplete="current-password"
+          fullWidth
+        />
+      )}
+    </>
   );
 };
 
