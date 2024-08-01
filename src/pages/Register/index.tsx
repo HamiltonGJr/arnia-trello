a52033d2c17f5ecc7e0a1abf85b85f3360eb9198
Hamilton Gonçalves <143231573+HamiltonGJr@users.noreply.github.com>
2024-08-01 14:45:15 -0300

    feat: including the handleOnClickRegister function, so we can register a user

--- src/pages/Register/index.tsx
+++ src/pages/Register/index.tsx
@@ -1,11 +1,41 @@
-import * as React from "react";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 import FormButton from "../../components/FormButton";
 import FormInput from "../../components/FormInput";
 import { FormBox, FormContainer, TitleArnia } from "./style";
 import { AppContainer } from "../../styles/BoryStyles";
+import api from "../../data/api";
 
 const Resgiter = () => {
+  const [fullName, setFullName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [repeatPassword, setRepeatPassword] = useState("");
+
+  const navigate = useNavigate();
+
+  const handleOnClickRegister = async () => {
+    if (password !== repeatPassword) {
+      console.log("Senha incorreta");
+      return;
+    }
+
+    try {
+      const user = await api.post("user", {
+        email: email,
+        name: fullName,
+        password: password,
+      });
+
+      if (user) {
+        return navigate("/");
+      }
+    } catch (error) {
+      console.log({ error: error });
+    }
+  };
+
   return (
     <AppContainer>
       <FormContainer component="form">
@@ -15,13 +45,27 @@ const Resgiter = () => {
         </FormBox>
 
         <FormBox>
-          <FormInput nameInput={"Nome Completo"} />
-          <FormInput nameInput={"E-mail"} />
-          <FormInput nameInput={"Senha"} isPassword />
-          <FormInput nameInput={"Repita a sua senha"} isPassword />
+          <FormInput
+            nameInput={"Nome Completo"}
+            onChange={(e) => setFullName(e.target.value)}
+          />
+          <FormInput
+            nameInput={"E-mail"}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <FormInput
+            nameInput={"Senha"}
+            isPassword
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <FormInput
+            nameInput={"Repita a sua senha"}
+            isPassword
+            onChange={(e) => setRepeatPassword(e.target.value)}
+          />
         </FormBox>
 
-        <FormButton>Cadastrar</FormButton>
+        <FormButton onClick={handleOnClickRegister}>Cadastrar</FormButton>
       </FormContainer>
     </AppContainer>
   );
