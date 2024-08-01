bd8ae912273207583c6549be9e6f06788782b6ac
Hamilton Gonçalves <143231573+HamiltonGJr@users.noreply.github.com>
2024-08-01 14:47:53 -0300

    feat: including the handleClick Login function, so we can log in with a user

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,25 +1,53 @@
-import * as React from "react";
-import { Link } from "react-router-dom";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 import FormButton from "../../components/FormButton";
 import FormInput from "../../components/FormInput";
-import { FormBox, FormContainer, TitleArnia } from "./style";
+import { FormBox, FormContainer, LinkBox, TitleArnia } from "./style";
 import { AppContainer } from "../../styles/BoryStyles";
+import api from "../../data/api";
 
 const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const navigate = useNavigate();
+
+  const handleOnClickLogin = async () => {
+    try {
+      const loginUser = await api.post("user/login", {
+        email: email,
+        password: password,
+      });
+
+      if (loginUser) {
+        return navigate("/todolist");
+      }
+    } catch (error) {
+      console.log({ error: error });
+    }
+  };
+
   return (
     <AppContainer>
       <FormContainer component="form">
         <TitleArnia variant="h3">Arnia Trello</TitleArnia>
 
         <FormBox>
-          <FormInput nameInput={"E-mail"} />
-          <FormInput nameInput={"Senha"} isPassword />
+          <FormInput
+            nameInput={"E-mail"}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <FormInput
+            nameInput={"Senha"}
+            isPassword
+            onChange={(e) => setPassword(e.target.value)}
+          />
         </FormBox>
 
         <FormBox>
-          <FormButton>Entrar</FormButton>
-          <Link to={"/register"}>Cadastre-se</Link>
+          <FormButton onClick={handleOnClickLogin}>Entrar</FormButton>
+          <LinkBox to={"/register"}>Cadastre-se</LinkBox>
         </FormBox>
       </FormContainer>
     </AppContainer>
