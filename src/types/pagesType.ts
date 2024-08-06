da875af6af459ae3e679f03c1da2b8d17f9c494d
Hamilton Gonçalves <143231573+HamiltonGJr@users.noreply.github.com>
2024-08-05 21:40:10 -0300

    feat: creating page typing

---
+++ src/types/pagesType.ts
@@ -0,0 +1,11 @@
+export type ILogin = {
+  email: string;
+  password: string;
+};
+
+export type IRegister = {
+  fullName: string;
+  email: string;
+  password: string;
+  repeatPassword: string;
+};
