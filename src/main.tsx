9fb462ab9c3387494816b531b1751f68c1985955
Hamilton Gonçalves <143231573+HamiltonGJr@users.noreply.github.com>
2024-08-06 15:00:32 -0300

    feat: including the ToastContainer component, so we have better error handling

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { CssBaseline } from "@mui/material";
 import { ThemeProvider } from "@mui/material";
+import { ToastContainer } from "react-toastify";
 
 import App from "./App.tsx";
 import theme from "./themes/theme.tsx";
@@ -10,6 +11,7 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <CssBaseline>
+        <ToastContainer />
         <App />
       </CssBaseline>
     </ThemeProvider>
