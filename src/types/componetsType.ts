12fe9ec183736c64975e5b81409c7af3dd18f55b
Hamilton Gonçalves <143231573+HamiltonGJr@users.noreply.github.com>
2024-08-05 21:42:19 -0300

    feat: inserting the typings to insert the react hook

--- src/types/componetsType.ts
+++ src/types/componetsType.ts
@@ -2,11 +2,14 @@ import { ChangeEvent } from "react";
 
 export type FormButtonProps = {
   children: React.ReactNode;
-  onClick: (e: React.FormEvent<HTMLFormElement>) => void;
+  onClick?: (e: React.FormEvent<HTMLFormElement>) => void;
+  type: string;
 };
 
 export type FormInputProps = {
   nameInput: string;
-  onChange: (e: ChangeEvent<HTMLInputElement>) => void;
+  onChange?: (e: ChangeEvent<HTMLInputElement>) => void;
   isPassword?: boolean;
+  error: boolean;
+  helperText?: string;
 };
